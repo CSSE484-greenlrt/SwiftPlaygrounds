61aa806576e0bcdf4a6aa8dd51d6ce49123b3423
Ryan Greenlee <Ryan@kimberlys-mbp.guest.rose-hulman.edu>
2018-03-08 12:29:12 -0500

    Worked on optionals

--- follow alongs/Optionals.playground/contents.swift
+++ follow alongs/Optionals.playground/contents.swift
@@ -1,3 +1,5 @@
+import UIKit
+
 // We already know about constants vs variables but there is another choice always present.
 var x = 7
 var f: Float = 7
@@ -6,29 +8,38 @@ let dave = "Dave"
 
 // Part 1.
 // Optionals
+var optionalFloat: Float?
+var requiredFloat: Float
+print("optionalFloat = \(optionalFloat)")
+//print("requireFloat = \(requiredFloat)")
 
-
-
-
+optionalFloat = 5
+requiredFloat = 5
+print("optionalFloat = \(optionalFloat)")
+print("requireFloat = \(requiredFloat)")
 
 // Optionals with forced unwrapping
+print("optionalFloat = \(optionalFloat!)")
 
+Int("4")
+Int("Dave")
 
-
-
-
-
-
-
-
+var optionalThatIsNil = Int("Dave")
+var optionalThatIsNotNil = Int("4")
+//print("optionalThatIsNil \(optionalThatIsNil!)")
+print("optionalThatIsNotNil \(optionalThatIsNotNil!)")
 
 // Part 2.
 // Views in a Playground + Optional Chaining
+let b = UIButton(frame: CGRect(origin: CGPoint(x: 0, y: 0),
+                               size: CGSize(width: 100, height: 100)))
+//b.setTitle("Press Me!", for: UIControlState.normal)
+b.setTitle("Press Me!", for: .normal)
+b.backgroundColor = .red
 
-
-
-
-
+b.titleLabel?.text
+b.titleLabel!.text!
+print("The title is \(b.titleLabel!.text!)")
 
 
 
