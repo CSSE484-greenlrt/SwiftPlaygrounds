1c10c7edc9bd9d7e037aaa8798290f612afb5f6c
Ryan Greenlee <Ryan@kimberlys-mbp.wlan.rose-hulman.edu>
2018-03-13 11:37:43 -0400

    classes

--- follow alongs/Classes.playground/contents.swift
+++ follow alongs/Classes.playground/contents.swift
@@ -19,7 +19,6 @@ class BankAccount: CustomStringConvertible {
     var description: String {
         return "\(name): $\(balance)"
     }
-    
 }
 
 var daveAccount = BankAccount(name: "Dave", balance: 100.00)
@@ -27,22 +26,40 @@ daveAccount.deposit(50)
 daveAccount.withdraw(10)
 
 
-
-
-
-
 // Subclass
+class AtmBankAccount: BankAccount {
+    var withdrawFee: Double = 0.0
+    let defaultWithdrawFee: Double = 2.0
+    
+    override init(name: String, balance: Double) {
+        withdrawFee = defaultWithdrawFee
+        super.init(name: name, balance: balance)
+    }
+    
+    init(name: String, balance: Double, withdrawFee: Double) {
+        self.withdrawFee = withdrawFee
+        super.init(name: name, balance: balance)
+    }
+    
+    convenience init() {
+        self.init(name: "Anonymous", balance: 0)
+    }
+    
+    override func withdraw(_ amount: Double) {
+        super.withdraw(amount)
+        balance -= withdrawFee
+    }
+}
+var defaultAccount = AtmBankAccount()
+defaultAccount.deposit(100)
+defaultAccount.withdraw(40)
 
 
+var bobAccount = AtmBankAccount(name: "Bob", balance: 10)
+bobAccount.deposit(100)
+bobAccount.withdraw(40)
 
 
-
-
-//var bobAccount = AtmBankAccount()
-//bobAccount.deposit(100)
-//bobAccount.withdraw(40)
-
-
-
-
-
+var expensiveAccount = AtmBankAccount(name: "Rich", balance: 10000, withdrawFee: 500)
+expensiveAccount.deposit(100)
+expensiveAccount.withdraw(40)
