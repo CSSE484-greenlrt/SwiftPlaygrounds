48cbc6dba3ca2928f677fe6e76fc68f457d6c5a5
Ryan Greenlee <Ryan@Kimberlys-MacBook-Pro.local>
2018-03-14 10:22:39 -0400

    Done with LightsOutGame Lab

--- follow alongs/TicTacToeGame.playground/contents.swift
+++ follow alongs/TicTacToeGame.playground/contents.swift
@@ -4,7 +4,7 @@ class TicTacToeGame:CustomStringConvertible {
         case oTurn = "O's Turn"
         case xWon = "X Wins!"
         case oWon = "O Wins!"
-        case tieGame = "Tie Game"
+        case tie = "Tie Game"
     }
     enum MarkType: String {
         case x = "X"
@@ -28,6 +28,44 @@ class TicTacToeGame:CustomStringConvertible {
         return gameString
     }
     
+    func pressedSquare(_ index: Int) {
+        if gameBoard[index] != .none {
+            return // This square is not empty
+        }
+        if gameState == .xTurn {
+            gameBoard[index] = .x
+            gameState = .oTurn
+            checkForGameOver()
+        } else if gameState == .oTurn {
+            gameBoard[index] = .o
+            gameState = .xTurn
+            checkForGameOver()
+        }
+    }
+    
+    func checkForGameOver() {
+        if !gameBoard.contains(.none) {
+            gameState = .tie
+        }
+        
+        var linesOf3 = [String]()
+        linesOf3.append(getGameString([0, 1, 2]))
+        linesOf3.append(getGameString([3, 4, 5]))
+        linesOf3.append(getGameString([6, 7, 8]))
+        linesOf3.append(getGameString([0, 3, 6]))
+        linesOf3.append(getGameString([1, 4, 7]))
+        linesOf3.append(getGameString([2, 5, 8]))
+        linesOf3.append(getGameString([0, 4, 8]))
+        linesOf3.append(getGameString([2, 4, 6]))
+        for lineOf3 in linesOf3 {
+            if lineOf3 == "XXX" {
+                gameState = .xWon
+            } else if lineOf3 == "OOO" {
+                gameState = .oWon
+            }
+        }
+    }
+    
     var description: String {
         return "\(gameState.rawValue) Board: \(getGameString())"
     }
@@ -36,24 +74,24 @@ class TicTacToeGame:CustomStringConvertible {
 
 /* ----------------- Official Playground testing ----------------- */
 var game = TicTacToeGame()
-//game.pressedSquare(0)
-//game.pressedSquare(1)
-//game.pressedSquare(3)
-//game.pressedSquare(2)
-//game.pressedSquare(6)
+game.pressedSquare(0)
+game.pressedSquare(1)
+game.pressedSquare(3)
+game.pressedSquare(2)
+game.pressedSquare(6)
 
 
 var game2 = TicTacToeGame()
 game2.gameBoard = [.x, .x, .o,
                 .none, .none, .none,
                 .o, .none, .none]
-//game2.pressedSquare(8)
-//game2.pressedSquare(4)
+game2.pressedSquare(8)
+game2.pressedSquare(4)
 
 
 var game3 = TicTacToeGame()
 game3.gameBoard = [.x, .x, .o,
     .o, .o, .x,
     .x, .o, .none]
-//game3.pressedSquare(8)
+game3.pressedSquare(8)
 
--- lights out lab/LinearLightsOutGame.playground/contents.swift
+++ lights out lab/LinearLightsOutGame.playground/contents.swift
@@ -1,30 +1,81 @@
 // TODO: Create the LinearLightsOutGame class
+import Foundation
 
+class LinearLightsOutGame:CustomStringConvertible {
+    var lightStates: [Bool]
+    var movesTaken: Int
+    var gameOver: Bool
+    
+    init(numLights: Int) {
+        lightStates = [Bool](repeating: false, count: numLights)
+        let randomNumber : Int = Int(arc4random_uniform(UInt32(numLights)))
+        for _ in 0..<randomNumber {
+            lightStates[Int(arc4random_uniform(UInt32(numLights)))] = true
+        }
+        movesTaken = 0
+        gameOver = false
+    }
+    
+    func pressedLightAtIndex(_ index: Int) -> Bool {
+        if gameOver {
+            return true
+        }
+        lightStates[index] = !lightStates[index]
+        if index == 0 {
+            lightStates[index + 1] = !lightStates[index + 1]
+        } else if index == lightStates.count - 1 {
+            lightStates[index - 1] = !lightStates[index - 1]
+        } else {
+            lightStates[index + 1] = !lightStates[index + 1]
+            lightStates[index - 1] = !lightStates[index - 1]
+        }
+        movesTaken += 1
+        for lightState in lightStates {
+            if lightState {
+                return false
+            }
+        }
+        gameOver = true
+        return true
+    }
+    
+    var description: String {
+        var result = "Lights: "
+        for lightState in lightStates {
+            if lightState {
+                result += "1"
+            } else {
+                result += "0"
+            }
+        }
+        return result + " Moves: \(movesTaken)"
+    }
+}
 
 
 /* ----------------- Official Playground testing ----------------- */
-//var lg = LinearLightsOutGame(numLights: 13)
-//lg.lightStates = [Bool](repeating: true, count: 13)
-//lg.pressedLightAtIndex(0)
-//lg
-//lg.pressedLightAtIndex(3)
-//lg
-//lg.pressedLightAtIndex(6)
-//lg
-//lg.pressedLightAtIndex(9)
-//lg
-//lg.pressedLightAtIndex(12)
-//lg
-//lg.pressedLightAtIndex(1)
-//lg
-//
-//
-//var lg2 = LinearLightsOutGame(numLights: 13)
-//lg2.lightStates = [true, true, false, false, false, false, false, false, false, false, true, true, true]
-//lg2.pressedLightAtIndex(0)
-//lg2
-//lg2.pressedLightAtIndex(11)
-//lg2
-//lg2.pressedLightAtIndex(6)
-//lg2
+var lg = LinearLightsOutGame(numLights: 13)
+lg.lightStates = [Bool](repeating: true, count: 13)
+lg.pressedLightAtIndex(0)
+lg
+lg.pressedLightAtIndex(3)
+lg
+lg.pressedLightAtIndex(6)
+lg
+lg.pressedLightAtIndex(9)
+lg
+lg.pressedLightAtIndex(12)
+lg
+lg.pressedLightAtIndex(1)
+lg
+
+
+var lg2 = LinearLightsOutGame(numLights: 13)
+lg2.lightStates = [true, true, false, false, false, false, false, false, false, false, true, true, true]
+lg2.pressedLightAtIndex(0)
+lg2
+lg2.pressedLightAtIndex(11)
+lg2
+lg2.pressedLightAtIndex(6)
+lg2
 
